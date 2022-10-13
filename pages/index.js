755ce822096d05954df668f80445fd086b65b0cd
MAHROOS ANWAR <mahroos.anwar0901@gmail.com>
2022-10-14 00:52:05 +0530

    use getServerSideProps insted of getStaticProps in indexjs

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Head from 'next/head';
+import Head from "next/head";
 import { MongoClient } from "mongodb";
 import { Fragment } from "react";
 
@@ -10,26 +10,17 @@ function HomePage(props) {
     <Fragment>
       <Head>
         <title>React Meetup</title>
-        <meta name='description' content='Browse a highly active React Meetups '/>
+        <meta
+          name="description"
+          content="Browse a highly active React Meetups "
+        />
       </Head>
       <MeetupList meetups={props.meetups} />
     </Fragment>
   );
 }
 
-// export async function getServerSideProps(context) {
-//   const req = context.req;
-//   const res = context.res;
-
-//   //fetch data from API
-//   return {
-//     props: {
-//       meetups: DUMMY_MEETUPS,
-//     },
-//   };
-// }
-
-export async function getStaticProps() {
+export async function getServerSideProps() {
   //fetch  data from API
 
   const client = await MongoClient.connect(
@@ -51,8 +42,33 @@ export async function getStaticProps() {
         id: meetup._id.toString(),
       })),
     },
-    revalidate: 300,
   };
 }
+
+// export async function getStaticProps() {
+//   //fetch  data from API
+
+//   const client = await MongoClient.connect(
+//     "mongodb+srv://mahroos:Onetwo12@cluster0.3gp3ow6.mongodb.net/?retryWrites=true&w=majority"
+//   );
+
+//   const db = client.db();
+
+//   const meetupsCollection = db.collection("meetups");
+
+//   const meetups = await meetupsCollection.find().toArray();
+//   client.close();
+//   return {
+//     props: {
+//       meetups: meetups.map((meetup) => ({
+//         title: meetup.title,
+//         address: meetup.address,
+//         image: meetup.image,
+//         id: meetup._id.toString(),
+//       })),
+//     },
+//     revalidate: 300,
+//   };
+// }
 
 export default HomePage;
